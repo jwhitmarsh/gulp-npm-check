aa46d8f62eb42956343d9fdf0662bce83306142a
James Whitmarsh <jwhitmarsh@dresources.com>
2016-04-11 20:36:40 +0100

    tests(main): updated tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,60 @@
 'use strict';
-const assert = require('chai').assert;
+const should = require('should');
 
 describe('gulp-npm-check', function() {
-  it('should run', function(done) {
+  it('should run when passed a callback', function(done) {
+    this.timeout(5000);
     let gulpNpmCheck = require('../lib/index');
     gulpNpmCheck((err, res) => {
-      if (err) {
-        console.log(err);
-        return done(err);
-      }
+      should.exists(err);
+      err.name.should.equal('NpmCheckError');
+      err.plugin.should.equal('gulp-npm-check');
       done();
+    });
+  });
+
+  it('should run when passed an object and a callback', function(done) {
+    this.timeout(5000);
+    let gulpNpmCheck = require('../lib/index');
+    gulpNpmCheck({}, (err, res) => {
+      try {
+        should.exists(err);
+        err.name.should.equal('NpmCheckError');
+        err.plugin.should.equal('gulp-npm-check');
+
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('should throw if config.throw is true', function(done) {
+    this.timeout(5000);
+    let gulpNpmCheck = require('../lib/index');
+    gulpNpmCheck({ throw: true }, (err, res) => {
+      try {
+        should.exists(err);
+        err.name.should.equal('NpmCheckError');
+        err.plugin.should.equal('gulp-npm-check');
+
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('should not throw if config.throw is false', function(done) {
+    this.timeout(5000);
+    let gulpNpmCheck = require('../lib/index');
+    gulpNpmCheck({ throw: false }, (err, res) => {
+      try {
+        should.not.exist(err);
+        done();
+      } catch (e) {
+        done(e);
+      }
     });
   });
 });
